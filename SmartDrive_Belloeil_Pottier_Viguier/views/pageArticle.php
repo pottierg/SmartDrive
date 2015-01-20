bcc63ee9c170c481a3a6ce24f915aa6b43aa13a4
EvansDev <evans-belloeil@hotmail.fr>
2015-01-20 10:51:19 +0100

    Debut page article

---
+++ SmartDrive_Belloeil_Pottier_Viguier/views/pageArticle.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+ 
+ <head>
+ 
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>IHM Drive</title>
+	<link rel="stylesheet" href="../css/foundation.css" />
+	<script src="js/vendor/modernizr.js"></script>
+
+</head>
+
+<body>
+<div class="nomArticle"><p><b>Nom Article</b></p></div>
+<div class="imageArticle"><p>Image</p></div>
+<div class="descriptionArticle"><p><i>Quid enim tam absurdum quam delectari multis inanimis rebus, 
+ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, 
+animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit.</i>.</p></div>
+<div class="boutonAjouter"> <button type="button">Click Me!</button> </div>
+</body>
+</html>
