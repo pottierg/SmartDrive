8c921868d3e5cbc2da9198f602bf352dff435db8
loicviguier <loicviguier@gmail.com>
2015-01-23 16:52:50 +0100

    #5 Choix Drive
    Implémentation accès BD

--- SmartDrive_Belloeil_Pottier_Viguier/databaseUtils/drive.sql
+++ SmartDrive_Belloeil_Pottier_Viguier/databaseUtils/drive.sql
@@ -2,10 +2,10 @@
 -- Query: 
 -- Date: 2015-01-22 22:18
 */
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (1,'HCI Drive',1);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (2,'HCI Drive',2);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (3,'HCI Drive',3);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (4,'HCI Drive',4);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (5,'HCI Drive',5);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (6,'HCI Drive',6);
-INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (7,'HCI Drive',7);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (1,'HCI Drive Toulouse',1);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (2,'HCI Drive Toulouse',2);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (3,'HCI Drive Toulouse',3);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (4,'HCI Drive Balma',4);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (5,'HCI Drive Balma',5);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (6,'HCI Drive Labege',6);
+INSERT INTO `Drive` (`idDrive`,`nomDrive`,`Adresse_idAdresse`) VALUES (7,'HCI Drive Labege',7);
