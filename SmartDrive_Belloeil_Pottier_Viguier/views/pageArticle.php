2988e6a21ac7dc35df4289f5fc9d95469f79ae62
EvansDev <evans-belloeil@hotmail.fr>
2015-01-26 10:10:08 +0100

    ajout CSS et page article

--- SmartDrive_Belloeil_Pottier_Viguier/views/pageArticle.php
+++ SmartDrive_Belloeil_Pottier_Viguier/views/pageArticle.php
@@ -7,22 +7,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>IHM Drive</title>
 	<link rel="stylesheet" href="../css/foundation.css" />
+	<link rel="stylesheet" href="../css/drivestyles.css" />
 	<script src="js/vendor/modernizr.js"></script>
 
 </head>
 
 <body>
-<div class="small-block-grid-8" >
+<div class="small-block-grid-7" >
 	
-	<li style="background-color:#E6E6FA">
-		<center>
-			<div class="nomArticle" style="text-decoration: underline"><p><b>Nom Article</b></p></div>
-			<div class="imageArticle"><p>Image</p></div>
-			<div class="descriptionArticle"><p><i>Quid enim tam absurdum quam delectari multis inanimis rebus, 
-			ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, 
-			animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit.</i>.</p></div>
-			<div class="boutonAjouter"> <button type="button">Click Me!</button> </div>
-		</center>
+	<li>
+		<div class="articleBackground1">
+			<center>
+				<div class="nomArticle" ><p><b>Nom Article</b></p></div>
+				<div class="imageArticle"><p>Image</p></div>
+				<div class="descriptionArticle"><p><i>Quid enim tam absurdum quam delectari multis inanimis rebus, 
+				ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, 
+				animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit.</i>.</p></div>
+				<div class="boutonAjouter"> <button type="button">Click Me!</button> </div>
+			</center>
+		</div>
 	</li>
 	
 </div>
