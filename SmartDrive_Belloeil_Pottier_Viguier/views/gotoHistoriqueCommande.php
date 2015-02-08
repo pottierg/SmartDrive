153a7f199a79d73965d6416f3ca7346c18c47381
loicviguier <loicviguier@gmail.com>
2015-02-08 13:44:10 +0100

    #13 Messages erreurs connexion

--- SmartDrive_Belloeil_Pottier_Viguier/views/gotoHistoriqueCommande.php
+++ SmartDrive_Belloeil_Pottier_Viguier/views/gotoHistoriqueCommande.php
@@ -2,13 +2,11 @@
 
 session_start();
 
-$_SESSION['titre'] = "HCI Drive - Panier";
+$_SESSION['titre'] = "HCI Drive - Historique des Commandes";
 $_SESSION['section'] = "./historiqueCommande.php";
-$_SESSION['nav'] = "Historique Commande";
+$_SESSION['nav'] = "Historique des Commandes";
 
 unset($_SESSION['navLink']);
 unset($_SESSION['nav2']);
 
 header("Location: ./gabarit.php");
-
-?>
